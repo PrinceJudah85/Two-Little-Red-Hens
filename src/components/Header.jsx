d4cb2b061e1c4b074646ce03e2fa394de44a1c8c
Rafael Castano <rafa328@gmail.com>
2019-11-19 17:18:34 -0500

    app.css updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/full-logo.png';
+import rectangleImage from '../images/rectangle.png';
 
 function Header() {
   return (
     <div className='header'>
       <div className='nav-home'>
         <Link to='/Home'>Home</Link>
+        <div className='header-underline'></div>
       </div>
       <div className='nav-menu'>
         <Link to='/Menu'>Menu</Link>
+        {/* <img className="menu-category-underline" src={rectangleImage} alt="underline"></img> */}
+        <div className='header-underline'></div>
       </div>
       <div className="nav-title">
         <Link to='/' className='nav-title'>
@@ -18,9 +22,13 @@ function Header() {
       </div>
       <div className='nav-about'>
         <Link to='/About'>About</Link>
+        <div className='header-underline'></div>
+        {/* <img className="menu-category-underline" src={rectangleImage} alt="underline"></img> */}
       </div>
       <div className='nav-contact'>
         <Link to='/ContactUs'>Contact Us</Link>
+        <div className='header-underline'></div>
+        {/* <img className="menu-category-underline" src={rectangleImage} alt="underline"></img> */}
       </div>
     </div>
   )
